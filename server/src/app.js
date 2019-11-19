93a6d80b165e6e82f1e0d711d4aaa889e1d1af38
lpeil <luan@peil.dev>
2019-11-19 11:58:10 -0300

    Create DB on Express

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,37 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const db = require('./database');
+require('dotenv').config();
 
-app.get('/', function (req, res) {
-  res.send('Opa!');
+//Import routes
+const authRoutes = require('./components/auth/routes');
+const userRoutes = require('./components/user/routes');
+
+//App
+const app = express();
+
+//Routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+
+//Test databese
+db.connect(function(err){
+  if(err) {
+    console.log('Error connecting to Db: ', err);
+  } else {
+    console.log('Connection established');
+  }
 });
 
+db.query('SHOW DATABASES', function(err, result) {
+  if (err) throw err;
+  console.log(result);
+});
+
+db.end();
+
+//Running app
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Running on port ${port}!`);
 });
-
---
+++ server/src/database.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+require('dotenv').config();
+  
+let db = mysql.createConnection({
+    host        : process.env.DB_HOST,
+    port        : process.env.DB_PORT,
+    database    : process.env.DB_DATABASE,
+    user        : process.env.DB_USER,
+    password    : process.env.DB_PASSWORD,
+});
+
+module.exports = db;
