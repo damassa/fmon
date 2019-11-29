5d98274268906db78d98234cbf3c956045d41c39
lpeil <luan@peil.dev>
2019-11-29 11:44:14 -0300

    :floppy_disk: Update database

--- backup/database.sql
+++ backup/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 21/11/2019 às 23:07
+-- Tempo de geração: 29/11/2019 às 11:42
 -- Versão do servidor: 5.7.28
 -- Versão do PHP: 7.3.6
 
@@ -30,8 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `images` (
   `id` int(11) NOT NULL,
-  `image` longtext NOT NULL,
-  `name` varchar(200) NOT NULL
+  `image` longtext NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -44,12 +43,24 @@ CREATE TABLE `news` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `image` int(12) NOT NULL,
-  `text` varchar(1250) NOT NULL,
+  `text` longtext NOT NULL,
   `author` int(11) NOT NULL,
   `likes` int(11) NOT NULL DEFAULT '0',
   `views` int(11) NOT NULL DEFAULT '0',
-  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `news_likes`
+--
+
+CREATE TABLE `news_likes` (
+  `id` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `news` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -88,6 +99,13 @@ ALTER TABLE `news`
   ADD KEY `image` (`image`);
 
 --
+-- Índices de tabela `news_likes`
+--
+ALTER TABLE `news_likes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `CLUSTERED` (`user`,`news`);
+
+--
 -- Índices de tabela `users`
 --
 ALTER TABLE `users`
@@ -109,6 +127,12 @@ ALTER TABLE `images`
 -- AUTO_INCREMENT de tabela `news`
 --
 ALTER TABLE `news`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `news_likes`
+--
+ALTER TABLE `news_likes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
